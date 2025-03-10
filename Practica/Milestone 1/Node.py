59aed285d845a4ef55e3039831e2253704eed440
nicxoxo <merayooooo@gmail.com>
2025-03-10 09:56:23 +0100

    Estructura components arbre

--- Practica/Milestone 1/Node.py
+++ Practica/Milestone 1/Node.py
@@ -2,6 +2,11 @@ from abc import ABC, abstractmethod
 
 class Node(ABC):
     @abstractmethod
+    def __init__(self):
+        """Constructor must be implemented by all subclasses"""
+        pass
+
+    @abstractmethod
     def predict(self, x):
         """Predict must be implemented by all subclasses"""
         pass
