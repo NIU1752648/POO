170618ce6a32611488b9f20ec96610cd958461ba
nicxoxo <merayooooo@gmail.com>
2025-05-11 20:00:12 +0200

    Diff multivariable inici

--- Exercicis/Exercici 2/DualMulti.py
+++ Exercicis/Exercici 2/DualMulti.py
@@ -2,4 +2,20 @@ import Dual
 import numpy as np
 
 class DualMulti:
-    pass
+    def __init__(self, a: np.array, b: np.array):
+        self._a = a
+        self._b = b
+
+    @property
+    def a(self):
+        return self._a
+
+    @property
+    def b(self):
+        return self._b
+
+    def __str__(self):
+        r = ""
+        for i in range(self.a):
+            r += f"<{self.a[i]} {self.b[i]} "
+        return r
--- Exercicis/Exercici 2/main.py
+++ Exercicis/Exercici 2/main.py
@@ -9,13 +9,31 @@ def gradient_ascent():
     h = 1e-3
     K = 1000
     values = []
-    for x_old in np.linspace(0, 2 * np.pi, 10):
+    for x_old in np.linspace(-2 * np.pi, 2 * np.pi, 10):
         x_new = x_old
         for _ in range(K):
             x_new += h * f(Dual(x_new, 1)).b
         values.append(x_new)
     return values
 
+def gradient_descent():
+    h = 1e-3
+    K = 1000
+    values = []
+    for x_old in np.linspace(-2 * np.pi, 2 * np.pi, 10):
+        x_new = x_old
+        for _ in range(K):
+            x_new -= h * f(Dual(x_new, 1)).b
+        values.append(x_new)
+    return values
+
 if __name__ == "__main__":
     x = gradient_ascent()
-    print(x)
+    y = gradient_descent()
+    for i in x: print(i)
+    for i in y: print(i)
+    x = np.linspace(-10, 10, 10)
+    y = (x ** 2) * (np.sin(x) ** 2 + np.cos(x))
+    plt.plot(x, y)
+    plt.title("Hola")
+    plt.show()
