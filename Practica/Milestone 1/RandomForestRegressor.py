264cd9fef8c326ed0cf07b2e6751c4e147ce0306
GEA <Gea.Schotte@autonoma.cat>
2025-05-12 10:00:06 +0200

    _make_leaf

--- Practica/Milestone 1/RandomForestRegressor.py
+++ Practica/Milestone 1/RandomForestRegressor.py
@@ -1,7 +1,13 @@
 from RandomForestClassifier import RandomForestClassifier
 from Impurity import Impurities
+from Dataset import Dataset
+from Tree import Tree
 
 class RandomForestRegressor(RandomForestClassifier):
     def __init__(self, num_trees = 100, ratio_samples = 0.8, max_depth = 10, impurity = Impurities.SumSquareError(), min_size = 1):
         super().__init__(num_trees, ratio_samples, max_depth, impurity, min_size)
+    
+    @staticmethod
+    def _make_leaf(dataset:Dataset):
+        return Tree.Leaf(dataset.mean_value())
 
